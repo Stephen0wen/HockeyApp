b0649a628cdbaa95b6b7dec941ead55d317f1875
miors <mior.sufian@googlemail.com>
2024-05-20 16:57:11 +0100

    setup done, now commencing seeding stage

---
+++ back-end/db/data/development-data/index.js
@@ -0,0 +1,5 @@
+exports.fixturesData = require("./fixtures.js");
+exports.responsesData = require("./responses.js");
+exports.rolesData = require("./roles.js");
+exports.teamsData = require("./teams.js");
+exports.venuesData = require("./venues.js");
---
+++ back-end/db/data/test-data/index.js
@@ -0,0 +1,5 @@
+exports.fixturesData = require("./fixtures.js");
+exports.responsesData = require("./responses.js");
+exports.rolesData = require("./role.js");
+exports.teamsData = require("./team.js");
+exports.venuesData = require("./venue.js");
