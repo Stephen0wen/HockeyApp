14fcf847ab18aa8199e66ced9393beeaee4d8614
Stephen0wen <stevejowen@gmail.com>
2024-05-21 16:45:05 +0100

    Moves MyFixtureUI into separate component

---
+++ front-end/Components/MyFixtureUI.js
@@ -0,0 +1,50 @@
+import { StyleSheet, View } from "react-native";
+import { SegmentedButtons, Text } from "react-native-paper";
+import { useState } from "react";
+
+export default function MyFixtureUI() {
+    const [value, setValue] = useState("");
+    // This will be replaced with the response stored in the database
+
+    const styles = StyleSheet.create({
+        container: {
+            marginTop: 10,
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "center",
+        },
+        button: {
+            flex: 2,
+            margin: 0,
+            paddingH: 0,
+            lineHeight: 20,
+            fontSize: 20,
+            width: 100,
+        },
+    });
+
+    return (
+        <View style={styles.container}>
+            <Text variant="labelMedium" style={styles.details}>
+                Details
+            </Text>
+            <SegmentedButtons
+                value={value}
+                onValueChange={setValue}
+                style={styles.button}
+                density="high"
+                buttons={[
+                    {
+                        value: "yes",
+                        label: "Yes",
+                    },
+                    {
+                        value: "maybe",
+                        label: "IDK",
+                    },
+                    { value: "no", label: "No" },
+                ]}
+            />
+        </View>
+    );
+}
--- front-end/Components/ResultsPage.js
+++ front-end/Components/ResultsPage.js
@@ -1,11 +1,8 @@
 import { ScrollView, StyleSheet, View } from "react-native";
-import { SegmentedButtons, Text } from "react-native-paper";
 import FixtureCard from "./FixtureCard";
-import { useState } from "react";
+import MyFixtureUI from "./MyFixtureUI";
 
 export default function ResultsPage() {
-    const [value, setValue] = useState("");
-
     const pastFixture = {
         fixture_id: 1,
         match_status: "completed",
@@ -37,62 +34,30 @@ export default function ResultsPage() {
     };
 
     const styles = StyleSheet.create({
-        container: {
-            marginTop: 10,
-            flexDirection: "row",
-            width: "100%",
-            justifyContent: "center",
-        },
         scroll: {
             flex: 1,
             alignItems: "center",
             gap: 5,
             padding: 5,
         },
-        button: {
-            flex: 2,
-            margin: 0,
-            paddingH: 0,
-            lineHeight: 20,
-            fontSize: 20,
-            width: 100,
-        },
+
         details: {
             flex: 1,
             marginHorizontal: 10,
         },
     });
 
-    const fixtureUI = (
-        <View style={styles.container}>
-            <Text variant="labelMedium" style={styles.details}>
-                Details
-            </Text>
-            <SegmentedButtons
-                value={value}
-                onValueChange={setValue}
-                style={styles.button}
-                density="high"
-                buttons={[
-                    {
-                        value: "yes",
-                        label: "Yes",
-                    },
-                    {
-                        value: "maybe",
-                        label: "IDK",
-                    },
-                    { value: "no", label: "No" },
-                ]}
-            />
-        </View>
-    );
-
     return (
         <ScrollView contentStyle={styles.scroll}>
-            <FixtureCard fixture={futureFixture}>{fixtureUI}</FixtureCard>
-            <FixtureCard fixture={futureFixture}>{fixtureUI}</FixtureCard>
-            <FixtureCard fixture={futureFixture}>{fixtureUI}</FixtureCard>
+            <FixtureCard fixture={futureFixture}>
+                <MyFixtureUI />
+            </FixtureCard>
+            <FixtureCard fixture={futureFixture}>
+                <MyFixtureUI />
+            </FixtureCard>
+            <FixtureCard fixture={futureFixture}>
+                <MyFixtureUI />
+            </FixtureCard>
             <FixtureCard fixture={futureFixture} />
             <FixtureCard fixture={pastFixture} />
         </ScrollView>
