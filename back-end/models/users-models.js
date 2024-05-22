93ca756a5943cf209e5c336196b339a4cb7fde5c
HanzoDaBoss <hanif.uddz@gmail.com>
2024-05-22 17:24:58 +0100

    Added testing, endpoint, model, controller for GET /api/users

---
+++ back-end/models/users-models.js
@@ -0,0 +1,20 @@
+const db = require("../db/connection");
+
+exports.selectUsers = () => {
+  return db
+    .query(
+      `SELECT users.user_id, users.team_id, user_name, team_name, player_bool, sec_bool, umpire_bool, organiser_bool, user_address_1, user_address_2, user_postcode, user_dob, user_phone FROM users JOIN roles ON users.user_id = roles.user_id JOIN teams ON users.team_id = teams.team_id;`
+    )
+    .then(({ rows }) => {
+      const userRows = rows.map((row) => {
+        row.user_role = [];
+        for (const [key, value] of Object.entries(row)) {
+          if (value === true) {
+            row.user_role.push(key);
+          }
+        }
+        return row;
+      });
+      return userRows;
+    });
+};
