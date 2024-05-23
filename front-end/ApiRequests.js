4136be4c23b754ce2f808d6decdf10f5859911e5
Stephen0wen <stevejowen@gmail.com>
2024-05-23 16:47:09 +0100

    Updates API request to access hosted version

--- front-end/ApiRequests.js
+++ front-end/ApiRequests.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getLeagueTables = () => {
     return axios
-        .get("http://localhost:9090/api/league_tables")
+        .get("https://hockeyapp.onrender.com/api/league_tables")
         .then(({ data: { league_tables } }) => {
             return league_tables;
         });
