e745f46053eb7366706b3343d3a64b68de9873db
Stephen0wen <stevejowen@gmail.com>
2024-05-24 16:14:17 +0100

    Fixes failing test

--- back-end/controllers/fixtures_byId.controller.js
+++ back-end/controllers/fixtures_byId.controller.js
@@ -1,10 +1,8 @@
 const { fetchFixturesById } = require("../models/fixtures_byId.model.js");
 
-const getFixturesById = (req, res, next) => {
-  const { fixture_id } = req.params;
-  fetchFixturesById(fixture_id)
-    .then((fixture) => res.status(200).send({ fixture }))
-    .catch(next);
+exports.getFixturesById = (req, res, next) => {
+    const { fixture_id } = req.params;
+    fetchFixturesById(fixture_id)
+        .then((fixture) => res.status(200).send({ fixture }))
+        .catch(next);
 };
-
-module.exports = getFixturesById;
