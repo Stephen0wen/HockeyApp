94e6d26184739ed7609fab0d368582d6b84b6f7e
HanzoDaBoss <hanif.uddz@gmail.com>
2024-05-24 17:24:33 +0100

    Removed duplicate responses router file

--- back-end/routes/responeses-router.js
+++
@@ -1,6 +0,0 @@
-const responsesRouter = require("express").Router();
-const { putResponse } = require("../controllers/responses-controllers");
-
-responsesRouter.put("/", putResponse);
-
-module.exports = responsesRouter;
--- back-end/routes/responses-router.js
+++ back-end/routes/responses-router.js
@@ -2,7 +2,9 @@ const responsesRouter = require("express").Router();
 const {
   getResponsesByUserId,
 } = require("../controllers/responses-byUserId.controller");
+const { putResponse } = require("../controllers/responses-controllers");
 
 responsesRouter.get("/:user_id", getResponsesByUserId);
+responsesRouter.put("/", putResponse);
 
 module.exports = responsesRouter;
