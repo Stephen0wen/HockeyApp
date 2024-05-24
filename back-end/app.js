9071d75b80fb2fd75d5b45b27ba0bdaec27b0ab6
miors <mior.sufian@googlemail.com>
2024-05-24 16:59:09 +0100

    readded code to resolve failing tests

--- back-end/app.js
+++ back-end/app.js
@@ -5,12 +5,17 @@ const {
   handleServerErrors,
 } = require("./errors/index.js");
 
+const {
+  getResponsesByUserId,
+} = require("./controllers/responses-byUserId.controller.js");
 const apiRouter = require("./routes/api-router");
 
 const app = express();
 app.use(express.json());
 
 app.use("/api", apiRouter);
+
+app.get("/api/responses/:user_id", getResponsesByUserId);
 
 app.all("*", (req, res, next) => {
   res.status(404).send({ msg: "Not Found" });
