5b1d12639e73cd4a9b836f1edea060fe8c8260a4
HanzoDaBoss <hanif.uddz@gmail.com>
2024-05-27 22:46:17 +0100

    Added endpoint, testing, error handling for PATCH /api/fixtures/:fixture_id

--- back-end/controllers/fixtures_byId.controller.js
+++ back-end/controllers/fixtures_byId.controller.js
@@ -1,8 +1,19 @@
-const { fetchFixturesById } = require("../models/fixtures_byId.model.js");
+const {
+  fetchFixturesById,
+  updateFixtureById,
+} = require("../models/fixtures_byId.model.js");
 
 exports.getFixturesById = (req, res, next) => {
-    const { fixture_id } = req.params;
-    fetchFixturesById(fixture_id)
-        .then((fixture) => res.status(200).send({ fixture }))
-        .catch(next);
+  const { fixture_id } = req.params;
+  fetchFixturesById(fixture_id)
+    .then((fixture) => res.status(200).send({ fixture }))
+    .catch(next);
+};
+
+exports.patchFixtureById = (req, res, next) => {
+  const { fixture_id } = req.params;
+  const { body } = req;
+  updateFixtureById(fixture_id, body)
+    .then((fixture) => res.status(200).send({ fixture }))
+    .catch(next);
 };
--- back-end/routes/fixtures-router.js
+++ back-end/routes/fixtures-router.js
@@ -1,9 +1,14 @@
 const fixturesRouter = require("express").Router();
 const { getFixtures } = require("../controllers/fixtures.controller");
-const { getFixturesById } = require("../controllers/fixtures_byId.controller");
+const {
+  getFixturesById,
+  patchFixtureById,
+} = require("../controllers/fixtures_byId.controller");
 
 fixturesRouter.get("/", getFixtures);
 
 fixturesRouter.get("/:fixture_id", getFixturesById);
+
+fixturesRouter.patch("/:fixture_id", patchFixtureById);
 
 module.exports = fixturesRouter;
