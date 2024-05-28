46b7ce04f6c6447ec5e92d513c0e6c4fe81e2a5c
ASP73 <alecsparker@outlook.com>
2024-05-28 16:47:26 +0100

    adds map functionality

--- front-end/Components/MyFixtures/MyFixturesList.js
+++ front-end/Components/MyFixtures/MyFixturesList.js
@@ -8,63 +8,68 @@ import MyFixtureCard from "./MyFixtureCard";
 import { MyFixtureContext } from "../../Contexts/MyFixtureContext";
 
 export default function MyFixturesList({ navigation }) {
-    const { user } = useContext(UserContext);
-    const [myFixtures, setMyFixtures] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const { setCurrentFixture } = useContext(MyFixtureContext);
+  const { user } = useContext(UserContext);
+  const [myFixtures, setMyFixtures] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { setCurrentFixture } = useContext(MyFixtureContext);
 
-    useEffect(() => {
-        setIsLoading(true);
-        getMyFixtures(user.team_id)
-            .then((apiMyFixtures) => {
-                setMyFixtures(apiMyFixtures);
-            })
-            .then(() => {
-                setIsLoading(false);
-            });
-    }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    getMyFixtures(user.team_id)
+      .then((apiMyFixtures) => {
+        setMyFixtures(apiMyFixtures);
+      })
+      .then(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-    if (isLoading) {
-        return <LoadScreen message="Loading your fixtures..." />;
-    }
+  if (isLoading) {
+    return <LoadScreen message="Loading your fixtures..." />;
+  }
 
-    return (
-        <>
-            <Text variant="headlineMedium" style={styles.title}>
-                My Fixtures
-            </Text>
-            <ScrollView contentStyle={styles.scroll}>
-                {myFixtures.map((fixture) => {
-                    return (
-                        <MyFixtureCard
-                            key={fixture.fixture_id}
-                            fixture={fixture}
-                        >
-                            <Button
-                                onPress={() => {
-                                    setCurrentFixture(fixture);
-                                    navigation.navigate("TeamSheet");
-                                }}
-                            >
-                                View Team Sheet
-                            </Button>
-                        </MyFixtureCard>
-                    );
-                })}
-            </ScrollView>
-        </>
-    );
+  return (
+    <>
+      <Text variant="headlineMedium" style={styles.title}>
+        My Fixtures
+      </Text>
+      <ScrollView contentStyle={styles.scroll}>
+        {myFixtures.map((fixture) => {
+          return (
+            <MyFixtureCard key={fixture.fixture_id} fixture={fixture}>
+              <Button
+                onPress={() => {
+                  setCurrentFixture(fixture);
+                  navigation.navigate("TeamSheet");
+                }}
+              >
+                View Team Sheet
+              </Button>
+              <Button
+                onPress={() => {
+                  setCurrentFixture(fixture);
+                  navigation.navigate("VenueMap");
+                }}
+              >
+                View Map of Venue
+              </Button>
+            </MyFixtureCard>
+          );
+        })}
+      </ScrollView>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    scroll: {
-        flex: 1,
-        alignItems: "center",
-        gap: 5,
-        padding: 5,
-    },
-    title: {
-        textAlign: "center",
-        margin: 5,
-    },
+  scroll: {
+    flex: 1,
+    alignItems: "center",
+    gap: 5,
+    padding: 5,
+  },
+  title: {
+    textAlign: "center",
+    margin: 5,
+  },
 });
