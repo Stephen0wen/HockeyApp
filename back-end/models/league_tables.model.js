245b8c5d7131238a4c3a1abebbb94203c744ca39
Stephen0wen <stevejowen@gmail.com>
2024-05-28 16:38:36 +0100

    Updates leaguetable endpoint so that it includes teams with no completed fixtures

--- back-end/models/league_tables.model.js
+++ back-end/models/league_tables.model.js
@@ -1,5 +1,28 @@
 const db = require("../db/connection");
 
+exports.selectCompletedFixtures = () => {
+    return db
+        .query(
+            `
+      SELECT
+        team1.team_name AS team1_name,
+        team2.team_name AS team2_name,
+        team1_score,
+        team2_score,
+        team1.team_division AS division
+      FROM fixtures
+      JOIN teams AS team1
+        ON fixtures.team1_id = team1.team_id
+      JOIN teams AS team2
+        ON fixtures.team2_id = team2.team_id
+      WHERE fixtures.match_status = 'completed' 
+  `
+        )
+        .then(({ rows }) => {
+            return rows;
+        });
+};
+
 exports.selectHomeFixtures = () => {
     return db
         .query(
