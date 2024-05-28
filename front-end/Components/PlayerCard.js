4984c4fe984ce6a0e28d08d1333830f743e03415
Stephen0wen <stevejowen@gmail.com>
2024-05-28 09:24:37 +0100

    Removes obsolete code

--- front-end/Components/PlayerCard.js
+++ front-end/Components/PlayerCard.js
@@ -1,35 +1,21 @@
 import { Card, Text } from "react-native-paper";
-import { StyleSheet, View } from "react-native";
-import { useColorScheme } from "react-native";
+import { StyleSheet } from "react-native";
 
 export default function PlayerCard({ player }) {
-    const colorScheme = useColorScheme();
-
-    // let backgroundColor = "";
-    // if (player.availability === "yes") {
-    //     backgroundColor = colorScheme === "dark" ? "#050" : "#afa";
-    // }
-    // if (player.availability === "maybe") {
-    //     backgroundColor = colorScheme === "dark" ? "#660" : "#ff9";
-    // }
-    // if (player.availability === "no") {
-    //     backgroundColor = colorScheme === "dark" ? "#500" : "#fbb";
-    // }
-
-    const styles = StyleSheet.create({
-        text: {
-            textAlign: "center",
-        },
-        card: {
-            width: 200,
-            padding: 5,
-            height: 30,
-        },
-    });
-
     return (
         <Card mode="contained" style={styles.card}>
             <Text style={styles.text}>{player.name} surname</Text>
         </Card>
     );
 }
+
+const styles = StyleSheet.create({
+    text: {
+        textAlign: "center",
+    },
+    card: {
+        width: 200,
+        padding: 5,
+        height: 30,
+    },
+});
