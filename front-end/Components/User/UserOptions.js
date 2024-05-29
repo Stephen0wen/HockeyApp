ff090ad35afb31ef9cc0427c7d70c360d1fa3ace
HanzoDaBoss <hanif.uddz@gmail.com>
2024-05-29 16:22:24 +0100

    Updated user role when deleting user

--- front-end/Components/User/UserOptions.js
+++ front-end/Components/User/UserOptions.js
@@ -66,6 +66,7 @@ export default function UserOptions({ visible, setVisible }) {
         toggleModalUserDelete={toggleModalUserDelete}
         user={user}
         setUser={setUser}
+        setUserRole={setUserRole}
       />
     </Portal>
   );
