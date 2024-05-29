a79799b4c9a5206d065d80bd56095df8771e22f5
Stephen0wen <stevejowen@gmail.com>
2024-05-29 10:35:15 +0100

    Creates custom colour schemes

--- front-end/index.js
+++ front-end/index.js
@@ -1,13 +1,15 @@
 import { registerRootComponent } from "expo";
-import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper";
+import { PaperProvider } from "react-native-paper";
 import { useColorScheme } from "react-native";
 import App from "./App";
 import { UserProvider } from "./Contexts/UserContext";
+import darkTheme from "./assets/themes/darkTheme";
+import lightTheme from "./assets/themes/lightTheme";
 
 export default function Main() {
     const colorScheme = useColorScheme();
 
-    const theme = colorScheme === "dark" ? MD3DarkTheme : MD3LightTheme;
+    const theme = colorScheme === "dark" ? darkTheme : lightTheme;
 
     return (
         <PaperProvider theme={theme}>
