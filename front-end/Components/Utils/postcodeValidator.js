71be6947315709f7d68ac680256f5b0e26eb1c6f
Alfie-F <alfie.fenables99Gmail.com>
2024-05-30 14:18:09 +0100

    adjusts regex

--- front-end/Components/Utils/postcodeValidator.js
+++ front-end/Components/Utils/postcodeValidator.js
@@ -1,5 +1,5 @@
 export function valid_postcode(postcode) {
   postcode = postcode.replace(/\s/g, "");
-  const regex = /^[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}$/i;
+  const regex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,2} ?[0-9][A-Z]{2}$/i;
   return regex.test(postcode);
 }
