05af8ebb2ca7053fb0573e6e6f5730043883eae3
Stephen0wen <stevejowen@gmail.com>
2024-05-30 01:24:31 +0100

    Styles all user popups

--- front-end/Components/User/UserLogOutPopup.js
+++ front-end/Components/User/UserLogOutPopup.js
@@ -1,92 +1,113 @@
 import { Modal, Text, useTheme, Button, Divider } from "react-native-paper";
 import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 export default function UserLogOutPopup({
-  visibleUserLogOff,
-  toggleModalUserLogOff,
-  user,
-  setUser,
-  setUserRole,
+    visibleUserLogOff,
+    toggleModalUserLogOff,
+    user,
+    setUser,
+    setUserRole,
 }) {
-  const theme = useTheme();
-  const containerStyle = {
-    backgroundColor: theme.colors.primaryContainer,
-    padding: 20,
-    width: "auto",
-    borderRadius: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    marginLeft: "10%",
-    marginRight: "10%",
-    display: "flex",
-  };
+    const theme = useTheme();
 
-  const [visibleUserLogOffSuccess, setVisibleUserLogOffSuccess] =
-    useState(false);
-  const toggleModalUserLogOffSuccess = () =>
-    setVisibleUserLogOffSuccess(!visibleUserLogOffSuccess);
+    const [visibleUserLogOffSuccess, setVisibleUserLogOffSuccess] =
+        useState(false);
+    const toggleModalUserLogOffSuccess = () =>
+        setVisibleUserLogOffSuccess(!visibleUserLogOffSuccess);
 
-  const handleUserLogOffYes = () => {
-    toggleModalUserLogOff();
-    toggleModalUserLogOffSuccess();
-  };
-  return (
-    <>
-      <Modal
-        visible={visibleUserLogOff}
-        onDismiss={toggleModalUserLogOff}
-        contentContainerStyle={containerStyle}
-        animationType="slide"
-      >
-        <Text>WARNING</Text>
-        <Text>Are you sure you want to log off?</Text>
-        <Button
-          width="100%"
-          marginBottom={"5%"}
-          marginTop={"10%"}
-          mode="outlined"
-          onPress={handleUserLogOffYes}
-        >
-          <Divider />
-          <Text>Yes</Text>
-        </Button>
-        <Button
-          width="100%"
-          marginBottom={"5%"}
-          marginTop={"10%"}
-          mode="outlined"
-          onPress={toggleModalUserLogOff}
-        >
-          <Divider />
-          <Text>No</Text>
-        </Button>
-      </Modal>
-      <Modal
-        visible={visibleUserLogOffSuccess}
-        onDismiss={() => {
-          toggleModalUserLogOffSuccess;
-          setUser(null);
-          setUserRole("public");
-        }}
-        contentContainerStyle={containerStyle}
-        animationType="slide"
-      >
-        <Text>You have logged off.</Text>
-        <Button
-          width="100%"
-          marginBottom={"5%"}
-          marginTop={"10%"}
-          mode="outlined"
-          onPress={() => {
-            toggleModalUserLogOffSuccess;
-            setUser(null);
-            setUserRole("public");
-          }}
-        >
-          <Divider />
-          <Text>Dismiss</Text>
-        </Button>
-      </Modal>
-    </>
-  );
+    const handleUserLogOffYes = () => {
+        toggleModalUserLogOff();
+        toggleModalUserLogOffSuccess();
+    };
+
+    const styles = StyleSheet.create({
+        modal: {
+            alignSelf: "center",
+            backgroundColor: theme.colors.primaryContainer,
+            borderRadius: 10,
+            marginHorizontal: 20,
+            alignItems: "center",
+            padding: 10,
+        },
+        button: {
+            width: 100,
+        },
+        buttonContainer: {
+            flexDirection: "row",
+            justifyContent: "center",
+            padding: 10,
+            gap: 10,
+        },
+        container: {
+            gap: 15,
+            alignItems: "center",
+            width: 240,
+        },
+    });
+
+    return (
+        <>
+            <Modal
+                visible={visibleUserLogOff}
+                onDismiss={toggleModalUserLogOff}
+                contentContainerStyle={styles.modal}
+                animationType="slide"
+            >
+                <View style={styles.container}>
+                    <Text>Are you sure you want to log out?</Text>
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            mode="elevated"
+                            onPress={handleUserLogOffYes}
+                            buttonColor={theme.colors.primary}
+                            textColor={theme.colors.onPrimary}
+                            style={styles.button}
+                        >
+                            Yes
+                        </Button>
+                        <Button
+                            mode="elevated"
+                            onPress={toggleModalUserLogOff}
+                            buttonColor={theme.colors.primary}
+                            textColor={theme.colors.onPrimary}
+                            style={styles.button}
+                        >
+                            No
+                        </Button>
+                    </View>
+                </View>
+            </Modal>
+            <Modal
+                visible={visibleUserLogOffSuccess}
+                onDismiss={() => {
+                    toggleModalUserLogOffSuccess;
+                    setUser(null);
+                    setUserRole("public");
+                }}
+                contentContainerStyle={styles.modal}
+                animationType="slide"
+            >
+                <View style={styles.container}>
+                    <Text>You have logged off.</Text>
+                    <Button
+                        width="100%"
+                        marginBottom={"5%"}
+                        marginTop={"10%"}
+                        mode="outlined"
+                        onPress={() => {
+                            toggleModalUserLogOffSuccess;
+                            setUser(null);
+                            setUserRole("public");
+                        }}
+                        buttonColor={theme.colors.primary}
+                        textColor={theme.colors.onPrimary}
+                        style={styles.button}
+                    >
+                        OK
+                    </Button>
+                </View>
+            </Modal>
+        </>
+    );
 }
