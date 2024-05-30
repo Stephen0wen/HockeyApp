6c8fa8ec6aadc58a4062f3ad072fb4f8912b203f
ASP73 <alecsparker@outlook.com>
2024-05-30 11:48:21 +0100

    adds add result functionality

--- front-end/Components/Navigators/SecretaryNavigator.js
+++ front-end/Components/Navigators/SecretaryNavigator.js
@@ -6,71 +6,64 @@ import HomePage from "../Home/HomePage";
 import ResultsPage from "../Results/ResultsPage";
 import MyFixturesPage from "../MyFixtures/MyFixturesPage";
 import { useTheme } from "react-native-paper";
-import PostResults from "../Secretary/PostResults";
+import SecretaryPage from "../Secretary/SecretaryPage";
 
 export default function SecretaryNavigator() {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const Tab = createMaterialBottomTabNavigator();
-    return (
-        <NavigationContainer theme={theme}>
-            <Tab.Navigator
-                initialRouteName="Home"
-                activeColor={theme.colors.primary}
-            >
-                <Tab.Screen
-                    name="Home"
-                    component={HomePage}
-                    options={{
-                        tabBarIcon: ({ color }) => (
-                            <MaterialCommunityIcons
-                                name="home"
-                                color={color}
-                                size={30}
-                            />
-                        ),
-                    }}
-                />
-                <Tab.Screen
-                    name="Results"
-                    component={ResultsPage}
-                    options={{
-                        tabBarIcon: ({ color }) => (
-                            <MaterialCommunityIcons
-                                name="scoreboard"
-                                color={color}
-                                size={30}
-                            />
-                        ),
-                    }}
-                />
-                <Tab.Screen
-                    name="My Fixtures"
-                    component={MyFixturesPage}
-                    options={{
-                        tabBarIcon: ({ color }) => (
-                            <MaterialCommunityIcons
-                                name="hockey-sticks"
-                                color={color}
-                                size={30}
-                            />
-                        ),
-                    }}
-                />
-                <Tab.Screen
-                    name="Secretary"
-                    component={PostResults}
-                    options={{
-                        tabBarIcon: ({ color }) => (
-                            <MaterialCommunityIcons
-                                name="briefcase-variant-outline"
-                                color={color}
-                                size={30}
-                            />
-                        ),
-                    }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
+  const Tab = createMaterialBottomTabNavigator();
+  return (
+    <NavigationContainer theme={theme}>
+      <Tab.Navigator initialRouteName="Home" activeColor={theme.colors.primary}>
+        <Tab.Screen
+          name="Home"
+          component={HomePage}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={30} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Results"
+          component={ResultsPage}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="scoreboard"
+                color={color}
+                size={30}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="My Fixtures"
+          component={MyFixturesPage}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="hockey-sticks"
+                color={color}
+                size={30}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Secretary"
+          component={SecretaryPage}
+          options={{
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons
+                name="briefcase-variant-outline"
+                color={color}
+                size={30}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
